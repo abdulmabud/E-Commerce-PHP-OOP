76ddab3cc650b5fd5b31ca51e8d9c20e7cbb1e49
abdulmabud <abdulmabudm@gmail.com>
2020-02-26 12:45:04 +0600

    Mastering frontend template

---
+++ footer.php
@@ -0,0 +1,15 @@
+
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Abdul Mabud 2020</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
