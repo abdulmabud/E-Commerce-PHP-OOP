bd66b587c2afcae949c4d00f3f7e6475773ca57f
abdulmabud <abdulmabudm@gmail.com>
2020-03-12 17:05:24 +0600

    Change homepage product view

--- index.php
+++ index.php
@@ -2,145 +2,163 @@
 <!-- Page Content -->
 <div class="container">
 
-<div class="row">
+  <div class="row">
 
 
 
-  <div class="col-lg-12">
+    <div class="col-lg-12">
 
-    <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
-      <ol class="carousel-indicators">
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-      </ol>
-      <div class="carousel-inner" role="listbox">
-        <div class="carousel-item active">
-          <img class="d-block img-fluid" src="http://placehold.it/1200x350" alt="First slide">
+      <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner" role="listbox">
+          <div class="carousel-item active">
+            <img class="d-block img-fluid" src="http://placehold.it/1200x350" alt="First slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block img-fluid" src="http://placehold.it/1200x350" alt="Second slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block img-fluid" src="http://placehold.it/1200x350" alt="Third slide">
+          </div>
         </div>
-        <div class="carousel-item">
-          <img class="d-block img-fluid" src="http://placehold.it/1200x350" alt="Second slide">
-        </div>
-        <div class="carousel-item">
-          <img class="d-block img-fluid" src="http://placehold.it/1200x350" alt="Third slide">
-        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
       </div>
-      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
+
+      <div class="row">
+
+        <div class="col-lg-3 col-md-6 mb-4">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="http://127.0.0.1:8000/product/1">Lincoln Schultz</a>
+              </h4>
+              <h5 class="d-inline"><del>BDT 254</del></h5>
+              <h5 class="d-inline">BDT 764</h5>
+            </div>
+            <div class="card-footer">
+              <a href="http://127.0.0.1:8000/cart/1" class="btn btn-primary btn-block">Add to Cart</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 mb-4">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="http://127.0.0.1:8000/product/1">Lincoln Schultz</a>
+              </h4>
+              <h5 class="d-inline"><del>BDT 254</del></h5>
+              <h5 class="d-inline">BDT 764</h5>
+            </div>
+            <div class="card-footer">
+              <a href="http://127.0.0.1:8000/cart/1" class="btn btn-primary btn-block">Add to Cart</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 mb-4">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="http://127.0.0.1:8000/product/1">Lincoln Schultz</a>
+              </h4>
+              <h5 class="d-inline"><del>BDT 254</del></h5>
+              <h5 class="d-inline">BDT 764</h5>
+            </div>
+            <div class="card-footer">
+              <a href="http://127.0.0.1:8000/cart/1" class="btn btn-primary btn-block">Add to Cart</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 mb-4">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="http://127.0.0.1:8000/product/1">Lincoln Schultz</a>
+              </h4>
+              <h5 class="d-inline"><del>BDT 254</del></h5>
+              <h5 class="d-inline">BDT 764</h5>
+            </div>
+            <div class="card-footer">
+              <a href="http://127.0.0.1:8000/cart/1" class="btn btn-primary btn-block">Add to Cart</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 mb-4">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="http://127.0.0.1:8000/product/1">Lincoln Schultz</a>
+              </h4>
+              <h5 class="d-inline"><del>BDT 254</del></h5>
+              <h5 class="d-inline">BDT 764</h5>
+            </div>
+            <div class="card-footer">
+              <a href="http://127.0.0.1:8000/cart/1" class="btn btn-primary btn-block">Add to Cart</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 mb-4">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="http://127.0.0.1:8000/product/1">Lincoln Schultz</a>
+              </h4>
+              <h5 class="d-inline"><del>BDT 254</del></h5>
+              <h5 class="d-inline">BDT 764</h5>
+            </div>
+            <div class="card-footer">
+              <a href="http://127.0.0.1:8000/cart/1" class="btn btn-primary btn-block">Add to Cart</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 mb-4">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="http://127.0.0.1:8000/product/1">Lincoln Schultz</a>
+              </h4>
+              <h5 class="d-inline"><del>BDT 254</del></h5>
+              <h5 class="d-inline">BDT 764</h5>
+            </div>
+            <div class="card-footer">
+              <a href="http://127.0.0.1:8000/cart/1" class="btn btn-primary btn-block">Add to Cart</a>
+            </div>
+          </div>
+        </div>
+
+
+
+      </div>
+      <!-- /.row -->
+
     </div>
-
-    <div class="row">
-
-      <div class="col-lg-3 col-md-6 mb-4">
-        <div class="card h-100">
-          <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-          <div class="card-body">
-            <h4 class="card-title">
-              <a href="#">Item One</a>
-            </h4>
-            <h5>$24.99</h5>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-          </div>
-          <div class="card-footer">
-            <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-3 col-md-6 mb-4">
-        <div class="card h-100">
-          <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-          <div class="card-body">
-            <h4 class="card-title">
-              <a href="#">Item Two</a>
-            </h4>
-            <h5>$24.99</h5>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div class="card-footer">
-            <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-3 col-md-6 mb-4">
-        <div class="card h-100">
-          <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-          <div class="card-body">
-            <h4 class="card-title">
-              <a href="#">Item Three</a>
-            </h4>
-            <h5>$24.99</h5>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-          </div>
-          <div class="card-footer">
-            <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-3 col-md-6 mb-4">
-        <div class="card h-100">
-          <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-          <div class="card-body">
-            <h4 class="card-title">
-              <a href="#">Item Four</a>
-            </h4>
-            <h5>$24.99</h5>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-          </div>
-          <div class="card-footer">
-            <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-3 col-md-6 mb-4">
-        <div class="card h-100">
-          <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-          <div class="card-body">
-            <h4 class="card-title">
-              <a href="#">Item Five</a>
-            </h4>
-            <h5>$24.99</h5>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div class="card-footer">
-            <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-3 col-md-6 mb-4">
-        <div class="card h-100">
-          <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-          <div class="card-body">
-            <h4 class="card-title">
-              <a href="#">Item Six</a>
-            </h4>
-            <h5>$24.99</h5>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-          </div>
-          <div class="card-footer">
-            <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-          </div>
-        </div>
-      </div>
-
-    </div>
-    <!-- /.row -->
+    <!-- /.col-lg-9 -->
 
   </div>
-  <!-- /.col-lg-9 -->
-
-</div>
-<!-- /.row -->
+  <!-- /.row -->
 
 </div>
 <?php include 'footer.php'; ?>
