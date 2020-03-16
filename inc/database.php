4c2b798b45f6821e6cbc9a12fe1b00f4a251f76d
abdulmabud <abdulmabudm@gmail.com>
2020-03-16 13:47:39 +0600

    Select Insert Upadte and Delete function added in database file

--- inc/database.php
+++ inc/database.php
@@ -19,6 +19,51 @@ class Database{
 
     }
 
+    // Select data 
+    public function select($query){
+        $result = $this->conn->query($query) or
+        die($this->conn->error . __LINE__);
+        if($result->num_rows > 0){
+            return $result;
+        }else{
+            return false;
+        }
+    }
+
+    // Insert data 
+    public function insert($query){
+        $insert_row = $this->conn->query($query) or
+        die($this->conn->error . __LINE__);
+        if($insert_row){
+            return $insert_row;
+        }else{
+            return false;
+        }
+    }
+
+    //Udpate data
+    public function update($query){
+        $update_row = $this->conn->query($query) or
+        die($this->conn->error . __LINE__);
+        if($update_row){
+            return $update_row;
+        }else{
+            return false;
+        }
+    }
+
+    //Delete data
+    public function delete($query){
+        $delete_row = $this->conn->query($query) or
+        die($this->conn->error . __LINE__);
+        if($delete_row){
+            return $delete_row;
+        }else{
+            return false;
+        }
+    }
+
+
 
 
 }
