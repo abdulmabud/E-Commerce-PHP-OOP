d2717b8393760ffd146ac46fa350be42aaa2e366
abdulmabud <abdulmabudm@gmail.com>
2020-06-15 12:43:53 +0600

    Custom JS file added

--- footer.php
+++ footer.php
@@ -9,6 +9,7 @@
   <!-- Bootstrap core JavaScript -->
   <script src="js/jquery.min.js"></script>
   <script src="js/bootstrap.bundle.min.js"></script>
+  <script src="js/custom.js"></script>
 
 </body>
 
